e1110f2c67076bd5732872f9dbc6184546ac6d16
Jose Stricklin <josestricklin@El-Diablo.local>
2018-11-03 13:44:49 -0600

    add dotenv

--- MenuDrawer/index.js
+++ MenuDrawer/index.js
@@ -4,6 +4,5 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import firebaseInit from './firebase.js'
-require('./credentials.env')
 
 AppRegistry.registerComponent(appName, () => App);
