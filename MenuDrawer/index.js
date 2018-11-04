69af13fa2ed195074e8582e773e9862822723e08
Jose Stricklin <josestricklin@El-Diablo.local>
2018-11-03 23:32:29 -0600

    working .env

--- MenuDrawer/index.js
+++ MenuDrawer/index.js
@@ -5,4 +5,6 @@ import App from './App';
 import {name as appName} from './app.json';
 import firebaseInit from './firebase.js'
 
+import { MQ_KEY, MQ_SEARCH_URL } from 'react-native-dotenv'
+
 AppRegistry.registerComponent(appName, () => App);
